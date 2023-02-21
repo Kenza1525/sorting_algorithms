bb3c7aa476521e2c34b909912f161162866c38c4
kenza1525 <kerianenzabampema@gmail.com>
2023-02-21 09:44:56 -0800

    Added 1-insertion_sort_list.c

---
+++ 1-insertion_sort_list.c
@@ -0,0 +1,31 @@
+#include "sort.h"
+/**
+ * insertion_sort_list - insertion sort doubly linked list
+ * @list: list
+ */
+void insertion_sort_list(listint_t **list)
+{
+	listint_t *tmp = NULL;
+
+	if (list == NULL)
+		return;
+	tmp = (*list)->next;
+	while (tmp != NULL)
+	{
+		while (tmp->prev != NULL && tmp->n < tmp->prev->n)
+		{
+			tmp->prev->next = tmp->next;
+			if (tmp->next != NULL)
+				tmp->next->prev = tmp->prev;
+			tmp->next = tmp->prev;
+			tmp->prev = tmp->next->prev;
+			tmp->next->prev = tmp;
+			if (tmp->prev == NULL)
+				*list = tmp;
+			else
+				tmp->prev->next = tmp;
+			print_list(*list);
+		}
+		tmp = tmp->next;
+	}
+}
