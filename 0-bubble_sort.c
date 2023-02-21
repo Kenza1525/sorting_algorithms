43a3b1e77d9beb3bc50251fed7cd79b0b3dfedda
kenza1525 <kerianenzabampema@gmail.com>
2023-02-21 05:45:03 -0800

    replaced i by j in the inner loop

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,55 +1,29 @@
 #include "sort.h"
 #include <stdio.h>
 
+/**
+ * bubble_sort - Sorts an array of integers
+ * in ascending order using Bubble sort
+ * @array: Pointer to the array to sort
+ * @size: Size of the array
+ */
 
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, j, a;
 
-	if (size == 0)
-        {
-                return;
-        }
-
 	for (i = 0; i < size - 1; i++)
 	{
 		for (j = 0; j < size - i - 1; j++)
 		{
-			if (array[i] > array[i+1])
-		       	{
-				a = array[i];
-				array[i] = array[i+1];
-				array[i+1] = a;
+			if (array[j] > array[j + 1])
+			{
+				a = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = a;
 				print_array(array, size);
 			}
 		}
 	}
 }
 
-/*
-int* bubble(int *array, size_t size)
-{
-        size_t i, a;
-
-        if (size == 0)
-        {
-                return;
-        }
-
-        if (array[i] > array[i+1])
-	{
-		a = array[i];
-		array[i] = array[i+1];
-		array[i+1] = a;
-		print_array(array, size);
-        }
-	return bubble(array, size - 1);
-}
-
-
-void bubble_sort(int *array, size_t size)
-{
-	int* a = bubble(array, size);
-	print_array(a);
-}
-*/
