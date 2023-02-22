15069ffc332cbd984247521a1500e3585e1efee9
kenza1525 <kerianenzabampema@gmail.com>
2023-02-22 12:47:02 -0800

    test2

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -31,7 +31,7 @@ void insertion_sort_list(listint_t **list)
 			}
 			temp->next = temp->prev;
 			temp->prev = temp->prev->prev;
-			temp->next->prev = temp;
+			temp->prev->prev = temp;
 			if (temp->prev == NULL)
 				*list = temp;
 			else
